0667fda1752704d62316e74eed67107fc3af3294
Danial Halim <danialhalim680@gmail.com>
2024-09-24 16:04:06 +0800

    Initial Commit

---
+++ LOOPS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LOOPS
+//
+//  Created by Danial Halim on 24/09/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
